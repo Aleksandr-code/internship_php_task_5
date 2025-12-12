289ad603aa092f100aa5690cc5eed5ade6a25ddc
Aleksandr-code <70668106+Aleksandr-code@users.noreply.github.com>
2025-12-12 17:17:01 +0500

    update max songns on page

--- public/assets/@symfony/stimulus-bundle/controllers-vi8nvrF.js
+++ public/assets/@symfony/stimulus-bundle/controllers-vi8nvrF.js
@@ -5,4 +5,4 @@ import controller_3 from "../../controllers/displayview_controller.js";
 import controller_4 from "../../controllers/hello_controller.js";
 export const eagerControllers = {"symfony--ux-turbo--turbo-core": controller_0, "autosubmit": controller_1, "details": controller_2, "displayview": controller_3, "hello": controller_4};
 export const lazyControllers = {"csrf-protection": () => import("../../controllers/csrf_protection_controller.js")};
-export const isApplicationDebug = true;
+export const isApplicationDebug = false;
